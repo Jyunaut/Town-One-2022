6ed50a6f1240c616eac09a21ad830656b2c5891a
CRAKAAW <crowshiding@gmail.com>
2022-04-30 11:26:03 -0700

    Updated GameManager with grid instance

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    public Grid<GridObject> grid;
+    public int gridWidth;
+    public int gridHeight;
+    public float gridCellSize;
+    public Vector3 gridOrigin;
 
     private void Awake()
     {
@@ -12,5 +17,7 @@ public class GameManager : MonoBehaviour
             Destroy(this);
         else
             Instance = this;
+
+        grid = new Grid<GridObject>(gridWidth, gridHeight, gridCellSize, gridOrigin, (Grid<GridObject> g, int x, int y) => new GridObject());
     }
 }
